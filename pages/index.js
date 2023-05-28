0fd92ee6c4c465236cb037b37611588002a329cc
jadashi97 <njadauniversal23@yahoo.com>
2023-05-28 12:15:55 -0500

    (feat): add profile section and style

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,10 @@ export default function Home(){
                 {/* header */}
                 <Header>
                     <UberLogo src="https://www.citypng.com/public/uploads/preview/uber-text-word-white-logo-png-116636032551bv8psou6e.png"/>
+                    <Profile>
+                        <Name>Lokose Dan</Name>
+                        <UserImage src="https://avatars.githubusercontent.com/u/95899616?s=400&u=d070ae1f6c50171b4c3c258a6070327d77218ca4&v=4" />
+                    </Profile>
                 </Header>
                 {/* Buttons */}
                 {/* InputButton */}
@@ -20,21 +24,38 @@ export default function Home(){
             </ActionItems>
         </Wrapper>
     )
-}
-
-
-const ActionItems = tw.div`
-    flex-1
-`
+};
 
 const Wrapper = tw.div`
     flex flex-col h-screen
-`
+
+`;
+
+
+const ActionItems = tw.div`
+    flex-1 p-4
+
+`;
 
 const Header = tw.div`
-
-`
+    flex justify-between items-center
+`;
 
 const UberLogo = tw.img`
-    h-28 bg-none
-`
+    h-28
+
+`;
+
+const Profile = tw.div`
+    flex items-center
+`;
+
+const Name = tw.div`
+    mr-4 w-20 text-sm
+`;
+
+
+
+const UserImage = tw.img`
+    h-16 w-16 rounded-full border border-gray-200 p-px
+`;
