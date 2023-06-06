9197883b648c9171032a2439973721acb4429a1f
jadashi97 <njadauniversal23@yahoo.com>
2023-06-05 21:01:57 -0500

    (feat): add login button

--- pages/login.js
+++ pages/login.js
@@ -6,13 +6,17 @@ const Login = () => {
     <Wrapper>
         <Uberlogo src="https://i.ibb.co/84stgjq/uber-technologies-new-20218114.jpg"/>
         <Title>
-            Login to Access your Account
+            Login to Access your account
         </Title>
+        <HeadImg src="https://i.ibb.co/CsV9RYZ/login-image.png"/>
+        <LoginButton>
+            Sign in with Google
+        </LoginButton>
     </Wrapper>
   )
 }
 
-export default Login
+export default Login;
 
 
 const Wrapper = tw.div`
@@ -20,9 +24,17 @@ const Wrapper = tw.div`
 `;
 
 const Uberlogo = tw.img`
-    h-20 object-contain  self-start w-auto
+    h-28 object-contain  self-start w-auto
 `;
 
 const Title = tw.div`
-    text-6xl
+    text-6xl text-center text-gray-500
+`;
+
+const HeadImg = tw.img`
+
+`;
+
+const LoginButton = tw.div`
+
 `;
