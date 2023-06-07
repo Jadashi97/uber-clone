20ae511827479ed03492bf2f7fefbcffbcfbabe5
jadashi97 <njadauniversal23@yahoo.com>
2023-06-07 11:40:23 -0500

    (feat): moved data and carlist to the public folder

--- pages/components/RideSelector.js
+++ pages/components/RideSelector.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import tw from 'tailwind-styled-components';
-import { carList } from '../data/carList';
+import { carList } from '../../public/data/carList';
 
 const RideSelector = ({pickupCoordinates, dropOffCoordinates}) => {
 
