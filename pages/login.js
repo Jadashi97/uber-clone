9b7cc9a6d09168f5f2fc21a0c6ee01fef5701bf0
jadashi97 <njadauniversal23@yahoo.com>
2023-06-10 10:25:32 -0500

    (fix): added company name and designed it

--- pages/login.js
+++ pages/login.js
@@ -19,12 +19,21 @@ const Login = () => {
 
     return (
         <Wrapper>
-            <YallaLogo 
-                src="https://cdn.pixabay.com/photo/2016/12/01/09/04/tuk-tuk-1874740_640.jpg"
-                onClick={() => signInWithPopup(auth, provider)}
-            />
+            <HeaderContainer>
+                <YallaLogo 
+                    src="https://cdn.pixabay.com/photo/2016/12/01/09/04/tuk-tuk-1874740_640.jpg"
+                    onClick={() => signInWithPopup(auth, provider)}
+                />
+                <CompanyName>
+                    YA-LA!
+                </CompanyName>
+                <YallaLogo2 
+                    src="https://cdn.pixabay.com/photo/2023/04/10/17/33/motorbike-7914313_640.png"
+                    onClick={() => signInWithPopup(auth, provider)}
+                />
+            </HeaderContainer>
             <Description>
-                Why wait ? Your Ride could be at Your Door in minutes!
+                Why wait ? Order & Ride with us!
             </Description>
             <Title>
                 Login to Access your YA-LA account!
@@ -44,20 +53,31 @@ const Wrapper = tw.div`
     flex flex-col h-screen w-full md: w-auto bg-white p-4
 `;
 
-const YallaLogo = tw.img`
-    h-32 object-contain  self-start w-auto bg-white transform hover:scale-105
+const HeaderContainer = tw.div`
+    flex flex-1 justify-between
 `;
 
+const YallaLogo = tw.img`
+    h-32 ml-6 object-contain  self-start w-auto bg-white transform hover:scale-105
+`;
+
+const YallaLogo2 = tw.img`
+    h-28 mr-6 object-contain  self-end w-auto bg-white transform hover:scale-105
+`;
+
+const CompanyName = tw.div`
+    text-6xl text-black p-4 mt-8 text-center bg-gradient-to-r from-purple-400 md:from-yellow-500
+`;
 const Title = tw.div`
-    text-4xl text-gray-500 p-8 text-center
+    text-3xl text-gray-500 p-8 text-center mb-8
 `;
 
 const Description = tw.div`
-    text-1.8xl text-gray-600 text-center p-6
+    text-2xl text-center p-6 font-style: italic text-yellow-600 
 `;
 
 const HeadImg = tw.img`
-    object-contain md: w-auto blur-sm transition ease-in-out delay-100 hover:scale-105 hover:blur-none
+    object-contain w-full blur-sm transition ease-in-out delay-100 hover:scale-105 hover:blur-none
 `;
 
 const LoginButton = tw.div`
